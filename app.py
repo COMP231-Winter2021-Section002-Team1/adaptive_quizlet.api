42bc020d5816b950a7fd9acb450d5c188f7a5e73
Hassan Shabbir <hassan149367@gmail.com>
2021-03-14 20:00:23 -0400

    scaffolded signup and signin pages

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home_page():
     return render_template('index.html')
 
+@app.route('/signup')
+def signup_page():
+    return 'This is the signup page'
+
+@app.route('/signin')
+def signin_page():
+    return 'This is the signin page'
 
 if __name__ == '__main__':
     app.run()
