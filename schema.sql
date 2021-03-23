a013edbf77f8e6a04b3f64862e0eb2cab7d9f1f8
Maanas Arora <maanasarora23@gmail.com>
2021-03-22 21:43:00 -0400

    add draft quiz pages and schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 -- SQLite3 db
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS quiz;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -8,5 +9,14 @@ CREATE TABLE user (
   password TEXT NOT NULL
 );
 
+CREATE TABLE quiz (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  author_id INTEGER FOREIGN KEY REFERENCES user(id)
+  quiz_type TEXT,
+  visibility BOOLEAN NOT NULL
+);
+
 -- Initialize data in db with example data
 INSERT INTO user VALUES (0, "hassan", "hassan149367@gmail.com", "password");
+INSERT INTO quiz VALUES (0, "Hassan's Trivia Quiz", 0, "Normal", TRUE);
