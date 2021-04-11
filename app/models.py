70a940fa8f13a0fda798f9d72bcca36fd707b385
saguoran <saguoran@gmail.com>
2021-04-11 15:00:51 -0400

    updated tables

--- app/models.py
+++ app/models.py
@@ -54,7 +54,7 @@ class QuizResult(db.Model):
 
 class Quiz(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
+    title = db.Column(db.Text, nullable=False)
     access_code = db.Column(db.String(80))
     limited_time = db.Column(db.Integer)
     posted_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
